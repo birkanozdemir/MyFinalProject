c6c006b3fde076341bea072992d1eaac9a59bd2e
Birkan Özdemir <birkan_ozdemir@hotmail.com>
2021-02-13 19:02:04 +0300

    10 Şubat Ders Sonu

---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    // interface başka bir interface i implemente ederse o interface in elemanları da
+    // o interface in içine aktarılmış olur
+    public interface IDataResult<T> : IResult
+    {
+        T Data { get; }
+    }
+}
