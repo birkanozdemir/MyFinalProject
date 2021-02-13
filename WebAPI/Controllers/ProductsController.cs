ac1054296102bd47dc8702710709b0c76e3b4f1a
Birkan Özdemir <birkan_ozdemir@hotmail.com>
2021-02-14 01:18:03 +0300

    13 Şubat Ders Sonu

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,67 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //--> Attribute (JAVA'da annotation'a karşılık gelir)
+    public class ProductsController : ControllerBase // Bu class bir Controller'dır
+    {
+        // Loosely coupled : gevşek bağımlılık
+        // naming convention
+        // IoC Container -- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            // Swagger --> Api ne zaman ne veriyor gibi dokümnatasyonlar sağlar
+            // Dependency chain
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
+
+// AddSingleton arka planda bir referans oluşturur. Bir bağımlılık görürsen karşılığı budur demektir.
+// singleton bellekte bir tane ProductManager oluşturur. bin tane client gelse de hepsine yanı
+// instance'ı verir. İçerisinde data tutmadığımızda kullanırız.
