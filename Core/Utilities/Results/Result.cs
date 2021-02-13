38b8894bd55fb953deb989d71e263128090d9a41
Birkan Özdemir <birkan_ozdemir@hotmail.com>
2021-02-13 16:14:31 +0300

    10 Şubat İlk Ders Sonu

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        // constructor da
+        // 1-sadece success döndürülmek isteniyorsa 
+        // 2- hem success hem message döndürülmek isteniyorsa this yapısı kullanılır.
+        // tek döndürülecek ayrı bir constructor yazılır. Her zaman istenilen veri buraya yazılır.
+        // İkisini de döndürmek isteniyorsa iki değişkenli constructor çalışır. this ile diğer 
+        // constructor içindeki veri otomatik olarak işleme dahil edilir.
+        public Result(bool success, string message) : this(success) 
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
